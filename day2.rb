16965a7df7eb6e67dc3b165e962e459e849ea51c
Maurice Codik <mcodik@gmail.com>
2021-12-06 19:07:49 -0500

    cleanup

--- day2.rb
+++ day2.rb
@@ -1,4 +1,34 @@
-require './submarine.rb'
+class Submarine
+    attr_reader :depth, :position, :aim
+
+    def initialize
+        @depth = 0
+        @position = 0
+        @aim = 0
+    end
+
+    def up(n)
+        @aim -= n
+        @aim
+    end
+
+    def down(n)
+        @aim += n
+        @aim
+    end
+
+    def forward(n)
+        @position += n
+        @depth += (@aim * n)
+        @position
+    end
+
+    def backward(n)
+        @position -= n
+        @depth -= (@aim * n)
+        @position
+    end
+end
 
 sub = Submarine.new
 File.readlines("day2.txt").each do |line|
--- hello.rb
+++
@@ -1,5 +0,0 @@
-def main()
-    puts "hello world!"
-end
-
-main()
