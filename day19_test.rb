57d50347d0f5f068b511e7d6876712b9ba1d7a6a
Maurice Codik <mcodik@gmail.com>
2021-12-19 23:12:40 -0500

    checkpoint

--- day19_test.rb
+++ day19_test.rb
@@ -35,14 +35,13 @@ class Day19Test < Test::Unit::TestCase
 
     def test_match_with_vectors_2
         scanners = parse("inputs/day19_test.txt")
-        adjusted_scanner, b1, b0 = match_with_vectors_2(scanners[0], scanners[1])
+        adjusted_scanner, b1, b0, scanner_loc = match_with_vectors_2(scanners[0], scanners[1])
         # best = vectors.keys.sort_by { |k| vectors[k] }
         # best.each do |b|
         #     puts "#{b.inspect} => #{vectors[b]}"
         # end
         # puts rotation
-        puts (b0+b1).inspect
-        puts b1.inspect
+        puts (scanner_loc).inspect
         puts "translation = #{adjusted_scanner.translation} rotation=#{adjusted_scanner.rotation}"
         puts adjusted_scanner.transformed_beacons.sort_by { |x| x[0] }.map { |v| v.inspect }
         puts "-----"
