3b232fb9c7a1012d964d5ed9d2195889a8d8e152
Maurice Codik <mcodik@gmail.com>
2022-12-10 18:49:12 -0800

    day9

--- aoc2022/src/day9.rs
+++ aoc2022/src/day9.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 
 #[derive(PartialEq)]
@@ -32,14 +32,14 @@ pub fn run_part_1(contents:&String) -> usize {
     for (dir, len) in cmds {
         for _ in 0..len {
             (head_i, head_j) = dir.next(head_i, head_j);
-            (tail_i, tail_j) = update_tail(&dir, tail_i, tail_j, head_i, head_j);
+            (tail_i, tail_j) = update_tail(tail_i, tail_j, head_i, head_j);
             visited.insert((tail_i, tail_j));
         }
     }
     visited.len()
 }
 
-fn update_tail(dir:&Direction, tail_i:i32, tail_j:i32, head_i:i32, head_j:i32) -> (i32, i32) {
+fn update_tail(tail_i:i32, tail_j:i32, head_i:i32, head_j:i32) -> (i32, i32) {
     let i_dist = (tail_i - head_i).abs();
     let j_dist = (tail_j - head_j).abs();
     let dist = i_dist + j_dist;
@@ -48,8 +48,18 @@ fn update_tail(dir:&Direction, tail_i:i32, tail_j:i32, head_i:i32, head_j:i32) -
         return (tail_i, tail_j);
     }
     
-    if (i_dist == 2 && j_dist == 0) || (i_dist == 0 && j_dist == 2) {
-        return dir.next(tail_i, tail_j);
+    if i_dist == 2 && j_dist == 0 {
+        if head_i > tail_i {
+            return (tail_i+1, tail_j);
+        } else {
+            return (tail_i-1, tail_j);
+        }
+    } else if i_dist == 0 && j_dist == 2 {
+        if head_j > tail_j {
+            return (tail_i, tail_j+1);
+        } else {
+            return (tail_i, tail_j-1);
+        }
     }
 
     let choices = vec![
@@ -59,67 +69,73 @@ fn update_tail(dir:&Direction, tail_i:i32, tail_j:i32, head_i:i32, head_j:i32) -
         (Direction::DOWN, Direction::RIGHT),
     ];
 
+    let mut min_dist = 10;
+    let mut min = (0,0);
     for choice in choices {
         let (i, j) = choice.0.next(tail_i, tail_j);
         let (i, j) = choice.1.next(i, j);
         let i_dist = (i - head_i).abs();
         let j_dist = (j - head_j).abs();
-        if (i_dist + j_dist) == 1 {
-            return (i, j);
+        if (i_dist + j_dist) < min_dist {
+            min = (i, j);
+            min_dist = i_dist + j_dist;
         } 
     }
-    panic!("unexpected");
-    // if (head_i-2 == tail_i && head_j-1 == tail_j) || (head_i-1 == tail_i && head_j+2 == tail_j) {
-    // /*
-    //   H . .
-    //   . . T  
-    //  */
-    //  /*
-    //   H .
-    //   . .
-    //   . T  
-    //  */
-    //     (tail_i-1, tail_j-1)
-    // } else if (head_i+2 == tail_i && head_j-1 == tail_j) || (head_i+1 == tail_i && head_j-2 == tail_j) {
-    // /*
-    //   T . .
-    //   . . H  
-    //  */
-    //  /*
-    //   T .
-    //   . .
-    //   . H  
-    //  */
-    //     (tail_i+1, tail_j+1)
-    // } else if (head_i+1 == tail_i && head_j-2 == tail_j) || (head_i+2 == tail_i && head_j-1 == tail_j) {
-    // /*
-    //   . . H
-    //   T . .  
-    //  */
-    //  /*
-    //   . H
-    //   . .
-    //   T .  
-    //  */
-    //     (tail_i-1, tail_j+1)
-    // } else if (head_i-2 == tail_i && head_j+1 == tail_j) || (head_i-1 == tail_i && head_j+2 == tail_j) {
-    // /*
-    //   . . T
-    //   H . .  
-    //  */
-    //  /*
-    //   . T
-    //   . .
-    //   H .  
-    //  */    
-    //     (tail_i+1, tail_j-1)
-    // } else {
-    //     panic!("unexpected");
-    // }
+    // dbg!(tail_i, tail_j, head_i, head_j);
+    // panic!("unexpected");
+    return min;
 }
 
-pub fn run_part_2(contents:&String) -> i32 {
-    0   
+pub fn run_part_2(contents:&String) -> usize {
+    let cmds = parse(contents);
+    let mut visited = HashSet::new();
+    let mut knots = vec![
+        (0,0),
+        (0,0),
+        (0,0),
+        (0,0),
+        (0,0),
+        (0,0),
+        (0,0),
+        (0,0),
+        (0,0),
+        (0,0),
+    ];
+    visited.insert(knots[9]);
+    for (dir, len) in cmds {
+        for _ in 0..len {
+            let (head_i, head_j) = knots[0];
+            let (head_i, head_j) = dir.next(head_i, head_j);
+            knots[0] = (head_i, head_j);
+            for n in 0..knots.len()-1 {
+                let (knot_i, knot_j) = knots[n];
+                let (next_knot_i, next_knot_j) = knots[n+1];
+                let (tail_i, tail_j) = update_tail(next_knot_i, next_knot_j, knot_i, knot_j);
+                knots[n+1] = (tail_i, tail_j);
+            } 
+            visited.insert(knots[9]);
+        }
+        // dump(&knots);
+    }
+    visited.len() 
+}
+
+fn dump(knots:&Vec<(i32, i32)>) {
+    let mut m = HashMap::new();
+    let dot = &'.'.to_string();
+    for (i, v) in knots.iter().rev().enumerate() {
+        let c = char::from_digit(i as u32, 10).unwrap().to_string();
+        m.insert(v, c);
+    }
+    for i in 0..6 {
+        let mut line = vec![];
+        for j in 0..7 {
+            let n = (i, j);
+            line.push(m.get(&n).unwrap_or(dot));
+        }
+        println!("{:?}", line);
+    }
+    println!("====");
 }
 
 fn parse(contents:&String) -> Vec<(Direction, i32)> {
